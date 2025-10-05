432a170a7ec985fbfa8a975d2ec027e0f6e0753e
RohanW-star <rohanwernars@gmail.com>
2025-10-05 11:42:14 +0200

    Applied Feedback

--- Assets/Scripts/Gameplay/UFOMovement.cs
+++ Assets/Scripts/Gameplay/UFOMovement.cs
@@ -1,67 +1,71 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class UFOMovement : MonoBehaviour
+namespace Player
 {
-    private Vector2 _moveInput;
-    private Rigidbody _ufoRigidbody;
-    private PlayerControls _controls;
-    private bool _canBoost = true;
-    private float speedboostCooldown = 5;
-
-    [SerializeField] private float _maxSpeed = 20f;
-    [SerializeField] private float _accelerateSpeed = 10f;
-
-    private void Awake()
+    public class UFOMovement : MonoBehaviour
     {
-        _controls = new PlayerControls();
-        _controls.UFOMovement.Move.performed += ctx => _moveInput = ctx.ReadValue<Vector2>();
-        _controls.UFOMovement.Move.canceled += ctx => _moveInput = Vector2.zero;
-        _controls.UFOMovement.SpeedBoost.performed += ctx => BoostSpeed();
-    }
+        [SerializeField] private float _maxSpeed = 20f;
+        [SerializeField] private float _accelerateSpeed = 10f;
+        [SerializeField] private float _currentCooldown = 5f;
 
-    void OnEnable() => _controls.Enable();
-    void OnDisable() => _controls.Disable();
+        private Vector2 _moveInput;
+        private Rigidbody _ufoRigidbody;
+        private PlayerControls _controls;
+        private bool _canBoost = true;
+        private float _SpeedboostCooldown = 5f;
+        private float _boostSpeed = 350f;
 
-    void Start()
-    {
-        _ufoRigidbody = GetComponent<Rigidbody>();
-    }
-
-    void FixedUpdate()
-    {
-        Vector3 movementDirection = new Vector3(_moveInput.x, 0, _moveInput.y);
-        if (movementDirection != Vector3.zero)
+        private void Awake()
         {
-            if (_ufoRigidbody.linearVelocity.magnitude > _maxSpeed)
-                _ufoRigidbody.linearVelocity = _ufoRigidbody.linearVelocity.normalized * _maxSpeed;
-            else
-                _ufoRigidbody.AddForce(movementDirection * _accelerateSpeed, ForceMode.Acceleration);
+            _controls = new PlayerControls();
+            _controls.UFOMovement.Move.performed += ctx => _moveInput = ctx.ReadValue<Vector2>();
+            _controls.UFOMovement.Move.canceled += ctx => _moveInput = Vector2.zero;
+            _controls.UFOMovement.SpeedBoost.performed += ctx => SpeedBoost();
         }
-        else
+
+        private void OnEnable() => _controls.Enable();
+        private void OnDisable() => _controls.Disable();
+
+        private void Start()
         {
-            _ufoRigidbody.AddForce(Vector3.zero);
+            _ufoRigidbody = GetComponent<Rigidbody>();
         }
-    }
 
-    private void Update()
-    {
-        speedboostCooldown = Mathf.Max(speedboostCooldown - Time.deltaTime, 0f);
-
-        if (speedboostCooldown <= 0)
-            _canBoost = true;
-        else
-            _canBoost = false;
-    }
-
-    void BoostSpeed()
-    {
-        if (_canBoost)
+        private void FixedUpdate()
         {
             Vector3 movementDirection = new Vector3(_moveInput.x, 0, _moveInput.y);
-            _ufoRigidbody.AddForce(movementDirection * 350, ForceMode.Acceleration);
 
-            speedboostCooldown = 5f;
+            if (movementDirection != Vector3.zero)
+            {
+                if (_ufoRigidbody.linearVelocity.magnitude > _maxSpeed)
+                    _ufoRigidbody.linearVelocity = _ufoRigidbody.linearVelocity.normalized * _maxSpeed;
+                else
+                    _ufoRigidbody.AddForce(movementDirection * _accelerateSpeed, ForceMode.Acceleration);
+            }
+            else
+                _ufoRigidbody.AddForce(Vector3.zero);
+        }
+
+        private void Update()
+        {
+            _currentCooldown = Mathf.Max(_currentCooldown - Time.deltaTime, 0f);
+
+            _canBoost = _currentCooldown <= 0;
+        }
+
+        private void SpeedBoost()
+        {
+            if (!_canBoost)
+            {
+                return;
+            }
+
+            Vector3 movementDirection = new Vector3(_moveInput.x, 0, _moveInput.y);
+            _ufoRigidbody.AddForce(movementDirection * _boostSpeed, ForceMode.Acceleration);
+
+            _currentCooldown = _SpeedboostCooldown;
         }
     }
 }
+
