252784a52d33d59a9581515ed3375e7ba24a3108
Bas de Reus <70578065+bas-boop@users.noreply.github.com>
2025-10-07 16:22:21 +0200

    Added grabbing and deliver system

---
+++ Assets/Scripts/Player/InputParser.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+    [RequireComponent(typeof(PlayerInput))]
+    public sealed class InputParser : MonoBehaviour
+    {
+        [SerializeField] private Grabber grabber;
+        
+        private PlayerInput _playerInput;
+        private InputActionAsset _inputActionAsset;
+        
+        private void Awake()
+        {
+            GetReferences();
+            Init();
+        }
+
+        private void Update()
+        {
+            // use for continues input reading
+        }
+
+        private void OnEnable() => AddListeners();
+
+        private void OnDisable() => RemoveListeners();
+
+        private void GetReferences()
+        {
+            _playerInput = GetComponent<PlayerInput>();
+        }
+
+        private void Init() => _inputActionAsset = _playerInput.actions;
+
+        private void AddListeners()
+        {
+            _inputActionAsset["Grapple"].performed += GrappleAction;
+        }
+
+        private void RemoveListeners()
+        {
+            _inputActionAsset["Grapple"].performed -= GrappleAction;
+        }
+        
+        #region Context
+
+        private void GrappleAction(InputAction.CallbackContext context) => grabber.DoGrab();
+
+        #endregion
+    }
+}
