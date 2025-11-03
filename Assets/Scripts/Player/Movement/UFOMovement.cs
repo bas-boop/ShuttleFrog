1110d289142d025a3a6bb5e17acecda39dba814c
Bas de Reus <70578065+bas-boop@users.noreply.github.com>
2025-11-03 23:26:23 +0100

    Added delceleration attribute

--- Assets/Scripts/Player/Movement/UFOMovement.cs
+++ Assets/Scripts/Player/Movement/UFOMovement.cs
@@ -6,53 +6,69 @@ namespace Player.Movement
 {
     public sealed class UFOMovement : MonoBehaviour
     {
-        [SerializeField] private AnimationController animationController;
+        private const float NO_VELOCITY_THRESHOLD = 0.2f;
+        private const float MAGNITUDE_CAP = 0.01f;
+        private const float MOVING_ANIMATION_CAP = 0.5f;
         
+        [SerializeField] private AnimationController animationController;
+
+        [Header("Attributes")]
         [SerializeField] private float maxSpeed = 20f;
-        [SerializeField] private float accelerateSpeed = 10f;
+        [SerializeField, Range(1, 100)] private float accelerateSpeed = 10f;
+        [SerializeField, Range(0.1f, 10)] private float decelerateSpeed = 10f;
         [SerializeField] private float boostSpeed = 350f;
         [SerializeField] private float currentCooldown = 5f;
 
         private Rigidbody _ufoRigidbody;
-        private Vector2 _moveInput;
         private bool _canBoost = true;
         private float _speedBoostCooldown = 5f;
-        
+
         private void Start() => _ufoRigidbody = GetComponent<Rigidbody>();
-        
+
         private void Update()
         {
-            animationController.PlayAnimation("Moving", _ufoRigidbody.linearVelocity.magnitude > 0.5f);
-            
+            animationController.PlayAnimation("Moving", _ufoRigidbody.linearVelocity.magnitude > MOVING_ANIMATION_CAP);
+
             currentCooldown = Mathf.Max(currentCooldown - Time.deltaTime, 0f);
             _canBoost = currentCooldown <= 0;
         }
-        
+
         public void Move(Vector2 input)
         {
             Vector3 moveDirection = transform.forward * input.y + transform.right * input.x;
 
-            if (moveDirection.sqrMagnitude <= 0.01f)
+            if (moveDirection.sqrMagnitude > MAGNITUDE_CAP)
+            {
+                moveDirection.Normalize();
+
+                if (_ufoRigidbody.linearVelocity.magnitude > maxSpeed)
+                    _ufoRigidbody.linearVelocity = _ufoRigidbody.linearVelocity.normalized * maxSpeed;
+                else
+                    _ufoRigidbody.AddForce(moveDirection * accelerateSpeed, ForceMode.Acceleration);
+                
+                return;
+            }
+
+            if (_ufoRigidbody.linearVelocity.sqrMagnitude <= MAGNITUDE_CAP)
                 return;
             
-            moveDirection.Normalize();
+            Vector3 force = -_ufoRigidbody.linearVelocity.normalized * decelerateSpeed;
+            _ufoRigidbody.AddForce(force, ForceMode.Acceleration);
 
-            if (_ufoRigidbody.linearVelocity.magnitude > maxSpeed)
-                _ufoRigidbody.linearVelocity = _ufoRigidbody.linearVelocity.normalized * maxSpeed;
-            else
-                _ufoRigidbody.AddForce(moveDirection * accelerateSpeed, ForceMode.Acceleration);
+            if (_ufoRigidbody.linearVelocity.magnitude < NO_VELOCITY_THRESHOLD)
+                _ufoRigidbody.linearVelocity = Vector3.zero;
         }
-
 
         public void SpeedBoost()
         {
+            return;
+            
             if (!_canBoost)
                 return;
 
-            Vector3 movementDirection = new (_ufoRigidbody.linearVelocity.x, 0, _ufoRigidbody.linearVelocity.y);
+            Vector3 movementDirection = _ufoRigidbody.linearVelocity.normalized;
             _ufoRigidbody.AddForce(movementDirection * boostSpeed, ForceMode.Acceleration);
             currentCooldown = _speedBoostCooldown;
         }
     }
 }
-
--- Assets/Scripts/Player/Visuals/VisualTurner.cs
+++ Assets/Scripts/Player/Visuals/VisualTurner.cs
@@ -8,14 +8,19 @@ namespace Player.Visuals
         [SerializeField] private float rotationSpeed = 10;
         [SerializeField] private float correction = 90;
 
+        private Vector3 _lastDirection = Vector3.forward;
+
         private void Update()
         {
             Vector3 moveDirection = rigidbody.linearVelocity;
-            
-            if (moveDirection.sqrMagnitude <= 0.001f)
+
+            if (moveDirection.sqrMagnitude > 0.5f)
+                _lastDirection = moveDirection.normalized;
+
+            if (_lastDirection.sqrMagnitude <= 0.001f)
                 return;
-            
-            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+
+            Quaternion targetRotation = Quaternion.LookRotation(_lastDirection);
             targetRotation *= Quaternion.Euler(0, correction, 0);
             transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
         }
